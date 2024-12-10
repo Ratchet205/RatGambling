ddea69d53522bdf0bfdcc4968800c713f2a6972c
WerbeName <werbename.btw@gmail.com>
2024-12-10 23:52:53 +0100

    added TopBorder
    added TopBorderPanel,
    added TopBorderButtons

--- RatGambling/RatGambling.Desktop/src/StdForm.Designer.cs
+++ RatGambling/RatGambling.Desktop/src/StdForm.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "StdForm";
+            pTopBorder = new Panel();
+            pBMin = new PictureBox();
+            pBExit = new PictureBox();
+            pTopBorder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pBMin).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pBExit).BeginInit();
+            SuspendLayout();
+            // 
+            // pTopBorder
+            // 
+            pTopBorder.BackColor = Color.FromArgb(228, 21, 75);
+            pTopBorder.Controls.Add(pBMin);
+            pTopBorder.Controls.Add(pBExit);
+            pTopBorder.Dock = DockStyle.Top;
+            pTopBorder.Location = new Point(0, 0);
+            pTopBorder.Name = "pTopBorder";
+            pTopBorder.Size = new Size(1620, 32);
+            pTopBorder.TabIndex = 0;
+            // 
+            // pBMin
+            // 
+            pBMin.Dock = DockStyle.Right;
+            pBMin.Location = new Point(1540, 0);
+            pBMin.Name = "pBMin";
+            pBMin.Size = new Size(40, 32);
+            pBMin.TabIndex = 1;
+            pBMin.TabStop = false;
+            pBMin.Click += pBMin_Click;
+            pBMin.Paint += pBMin_Paint;
+            pBMin.MouseEnter += pBMin_MouseEnter;
+            pBMin.MouseLeave += pBMin_MouseLeave;
+            // 
+            // pBExit
+            // 
+            pBExit.Dock = DockStyle.Right;
+            pBExit.Location = new Point(1580, 0);
+            pBExit.Name = "pBExit";
+            pBExit.Size = new Size(40, 32);
+            pBExit.TabIndex = 0;
+            pBExit.TabStop = false;
+            pBExit.Click += pBExit_Click;
+            pBExit.Paint += pBExit_Paint;
+            pBExit.MouseEnter += pBExit_MouseEnter;
+            pBExit.MouseLeave += pBExit_MouseLeave;
+            // 
+            // StdForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            ClientSize = new Size(1620, 1020);
+            Controls.Add(pTopBorder);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "StdForm";
+            Text = "StdForm";
+            pTopBorder.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pBMin).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pBExit).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pTopBorder;
+        private PictureBox pBMin;
+        private PictureBox pBExit;
     }
 }
--- RatGambling/RatGambling.Desktop/src/StdForm.cs
+++ RatGambling/RatGambling.Desktop/src/StdForm.cs
@@ -12,9 +12,104 @@ namespace RatGambling.Desktop.src
 {
     public partial class StdForm : Form
     {
+        private bool pBExitHover = false;
+        private bool pBMinHover = false;
         public StdForm()
         {
             InitializeComponent();
+        }
+        private void pBExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pBExit_Paint(object sender, PaintEventArgs e)
+        {
+            if (sender is PictureBox pb)
+            {
+                Graphics g;
+                int x = pb.Width;
+                int y = pb.Height;
+
+                (int X, int Y) middle = (x / 2, y / 2);
+
+                g = e.Graphics;
+
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                pb.BackColor = pBExitHover ? Color.FromArgb(0xBE, 0, 0) : pTopBorder.BackColor;
+
+
+                using Pen pen = new(Color.FromArgb(0xbe, 0xbe, 0xbe), 1.6f)
+                {
+                    StartCap = System.Drawing.Drawing2D.LineCap.Round,
+                    EndCap = System.Drawing.Drawing2D.LineCap.Round
+                };
+                const int armLength = 5;
+                g.DrawLine(pen, middle.X - armLength, middle.Y + armLength, middle.X + armLength, middle.Y - armLength);
+                g.DrawLine(pen, middle.X + armLength, middle.Y + armLength, middle.X - armLength, middle.Y - armLength);
+            }
+        }
+
+        private void pBExit_MouseEnter(object sender, EventArgs e)
+        {
+            PictureBox? pictureBox = sender as PictureBox;
+            pBExitHover = true;
+            pictureBox?.Invalidate();
+        }
+
+        private void pBExit_MouseLeave(object sender, EventArgs e)
+        {
+            PictureBox? pictureBox = sender as PictureBox;
+            pBExitHover = false;
+            pictureBox?.Invalidate();
+        }
+
+        private void pBMin_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox? pb = sender as PictureBox;
+            if (pb != null)
+            {
+                Graphics g;
+                int x = pb.Width;
+                int y = pb.Height;
+
+                (int X, int Y) middle = (x / 2, y / 2);
+
+                g = e.Graphics;
+
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                pb.BackColor = pBMinHover ? Color.FromArgb(0xEF2056) : pTopBorder.BackColor;
+
+
+                using Pen pen = new(Color.FromArgb(0xbe, 0xbe, 0xbe), 1.6f)
+                {
+                    StartCap = System.Drawing.Drawing2D.LineCap.Round,
+                    EndCap = System.Drawing.Drawing2D.LineCap.Round
+                };
+                const int armLength = 5;
+                g.DrawLine(pen, middle.X - armLength, middle.Y, middle.X + armLength, middle.Y);
+            }
+        }
+
+        private void pBMin_MouseLeave(object sender, EventArgs e)
+        {
+            PictureBox? pictureBox = sender as PictureBox;
+            pBMinHover = false;
+            pictureBox?.Invalidate();
+        }
+
+        private void pBMin_MouseEnter(object sender, EventArgs e)
+        {
+            PictureBox? pictureBox = sender as PictureBox;
+            pBMinHover = true;
+            pictureBox?.Invalidate();
+        }
+
+        private void pBMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
